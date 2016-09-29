d3f89d476956e36e048a920c764daf9d440265b5
mundtandy <mundtandy@ecs.vuw.ac.nz>
2016-09-29 17:27:41 +1300

    Replace GamePanel.java with more generic UIPanel

--- src/com/school/ui/GamePanel.java
+++ src/com/school/ui/GamePanel.java
@@ -1,5 +1,6 @@
 package com.school.ui;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.ImageIcon;
@@ -7,25 +8,41 @@ import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class UIPanel extends JPanel {
 	private JPanel renderPanel;
+	private int width;
+	private int height;
+	private int panelNum;
 			
-	public GamePanel(JPanel panel){
+	public UIPanel(JPanel panel, int num ){
 		super();
+		this.panelNum = num;
 		JLayeredPane layers = new JLayeredPane();
 		layers.setLayout(null);
-		layers.setPreferredSize(new Dimension(364, 364));
+		
+		switch(num){
+			case 1 : 	width = 364;
+						height = 364;
+						break;
+			case 2 : 	width = 262;
+						height = 162;
+						break;
+			default :	width = 0;
+						height = 0;
+						break;
+		}
+		layers.setPreferredSize(new Dimension(width, height));
 		
 		//gets background image and assigns it to JLabel background
-		JLabel background = new JLabel(new ImageIcon(this.getClass().getResource("/com/school/ui/UIImages/1.png")));
-		background.setBounds(0, 0, 364, 364);			
+		JLabel background = new JLabel(new ImageIcon(this.getClass().getResource("/com/school/ui/UIImages/"+panelNum+".png")));
+		background.setBounds(0, 0, width, height);			
 		layers.add(background, new Integer(0), 0);
 	
 		//assigns render panel and places in correctly
 		this.renderPanel = panel;
-		renderPanel.setBounds(6,6,352,352);
+		renderPanel.setBounds(6,6,(width-12),(height-12));
 		layers.add(renderPanel, new Integer(1), 0);
-		
+		setOpaque(false);
 		this.add(layers);
 		this.setVisible(true);
 		validate();
