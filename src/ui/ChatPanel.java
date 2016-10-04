5bbb2f73bf85725ba27cc81640cca7ad65d88bd8
mundtandy <mundtandy@ecs.vuw.ac.nz>
2016-10-04 22:33:03 +1300

    chat colorisation

--- src/ui/ChatPanel.java
+++ src/ui/ChatPanel.java
@@ -17,89 +17,122 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultCaret;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 import network.Client;
 
 public class ChatPanel extends JPanel{
 	private JTextField typeArea;
 	private JButton sendMessage;
-	private JTextArea messageList;
+	private JTextPane messageList;
 	private GameFrame home;
 	private String playerName;
 	private Client client;
-	
+	private StyledDocument styled;
+	private Style message;
+	private Style systemMessage;
+
 	public ChatPanel(GameFrame display, String playerName, Client client) {
 		super(new BorderLayout());
 		this.setOpaque(true);
 		home = display;
 		this.playerName = playerName;
 		this.client = client;
-        this.setSize(288, 488);
-        JPanel southPanel = new JPanel();
-        southPanel.setBackground(Color.BLACK);
-        southPanel.setLayout(new GridBagLayout());
+		this.setSize(288, 488);
+		JPanel southPanel = new JPanel();
+		southPanel.setBackground(Color.BLACK);
+		southPanel.setLayout(new GridBagLayout());
 
-        typeArea = new JTextField(30);
-      
-        sendMessage = new JButton("Send");
-        sendMessage.addActionListener(new sendMessageButtonListener());
+		typeArea = new JTextField(30);
 
-        messageList = new JTextArea();
-        messageList.setEditable(false);
-        messageList.setOpaque(true);
-        try {
-    		Font font = Font.createFont(Font.TRUETYPE_FONT, getClass().getResourceAsStream("slkscr.ttf"));
-    		typeArea.setFont(font.deriveFont(Font.TRUETYPE_FONT, 12f));
-    		messageList.setFont(font.deriveFont(Font.TRUETYPE_FONT, 12f));
-    		
-    	} catch (Exception e) {}
-        
-        messageList.setLineWrap(true);
-        JScrollPane area = new JScrollPane(messageList);
-        
-        this.add(area, BorderLayout.CENTER);
+		sendMessage = new JButton("Send");
+		sendMessage.addActionListener(new sendMessageButtonListener());
 
-        GridBagConstraints left = new GridBagConstraints();
-        left.anchor = GridBagConstraints.LINE_START;
-        left.fill = GridBagConstraints.HORIZONTAL;
-        left.weightx = 512.0D;
-        left.weighty = 1.0D;
+		messageList = new JTextPane();
+		messageList.setEditable(false);
+		messageList.setOpaque(true);
+		DefaultCaret caret = (DefaultCaret)messageList.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		
+		styled = messageList.getStyledDocument();
 
-        GridBagConstraints right = new GridBagConstraints();
-        right.insets = new Insets(0, 10, 0, 0);
-        right.anchor = GridBagConstraints.LINE_END;
-        right.fill = GridBagConstraints.NONE;
-        right.weightx = 1.0D;
-        right.weighty = 1.0D;
+		message = messageList.addStyle("Message Style", null);
+		StyleConstants.setForeground(message, Color.BLACK);
+		systemMessage = messageList.addStyle("System Message", null);
+		StyleConstants.setForeground(systemMessage, Color.RED);
 
-        southPanel.add(typeArea, left);
-        southPanel.add(sendMessage, right);
 
-        this.add(BorderLayout.SOUTH, southPanel);
 
-        this.setVisible(true);
-    }
-	
-	public void addChange(List<String> changes){
-		for(String change: changes)
-			messageList.append(change+"\n");
+
+		try {
+			Font font = Font.createFont(Font.TRUETYPE_FONT, getClass().getResourceAsStream("slkscr.ttf"));
+			typeArea.setFont(font.deriveFont(Font.TRUETYPE_FONT, 12f));
+			messageList.setFont(font.deriveFont(Font.TRUETYPE_FONT, 12f));
+
+		} catch (Exception e) {}
+
+		// messageList.setLineWrap(true);
+		JScrollPane area = new JScrollPane(messageList);
+
+		this.add(area, BorderLayout.CENTER);
+
+		GridBagConstraints left = new GridBagConstraints();
+		left.anchor = GridBagConstraints.LINE_START;
+		left.fill = GridBagConstraints.HORIZONTAL;
+		left.weightx = 512.0D;
+		left.weighty = 1.0D;
+
+		GridBagConstraints right = new GridBagConstraints();
+		right.insets = new Insets(0, 10, 0, 0);
+		right.anchor = GridBagConstraints.LINE_END;
+		right.fill = GridBagConstraints.NONE;
+		right.weightx = 1.0D;
+		right.weighty = 1.0D;
+
+		southPanel.add(typeArea, left);
+		southPanel.add(sendMessage, right);
+
+		this.add(BorderLayout.SOUTH, southPanel);
+
+		this.setVisible(true);
 	}
-	
+
+	public void addChange(List<String> changes){
+		for(String change: changes){
+			if(change != null){
+				if(change.length() > 0){
+					if(change.charAt(0) == '<'){
+						try { styled.insertString(styled.getLength(), change+"\n" ,message); }
+						catch (BadLocationException e){}
+					} else {
+						try { styled.insertString(styled.getLength(), change+"\n" ,systemMessage); }
+						catch (BadLocationException e){}
+					}
+				}
+			}
+		}
+	}
+
 	public Client getClient(){
 		return client;
 	}
-	
-    class sendMessageButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            if (typeArea.getText().length() < 1) {
-                // do nothing
-            } else {
-            	String chat = "CHAT :  " + typeArea.getText();
-                typeArea.setText("");
-                
-                getClient().sendCommand(chat);
-            }
-            home.refocus();
-        }
-    }
+
+	class sendMessageButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			if (typeArea.getText().length() < 1) {
+				// do nothing
+			} else {
+				String chat = "CHAT :  " + typeArea.getText();
+				typeArea.setText("");
+
+				getClient().sendCommand(chat);
+			}
+			home.refocus();
+		}
+	}
 }
