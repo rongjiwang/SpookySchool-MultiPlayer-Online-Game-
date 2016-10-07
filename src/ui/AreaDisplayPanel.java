73d2437ff6d4a8db2370927a9b8fb9b7a127f9eb
tezz99 <tezz995@gmail.com>
2016-10-07 14:27:48 +1300

    fixed fence again

--- src/ui/AreaDisplayPanel.java
+++ src/ui/AreaDisplayPanel.java
@@ -285,9 +285,9 @@ public class AreaDisplayPanel extends JPanel implements KeyListener, MouseListen
 
 		// Draw Walls(Back and side walls with layer 1, front with layer 3)
 
-		if (((token.equals("w0") || token.equals("W1") || token.equals("f0") || token.equals("F1") || token.equals("B0")
-				|| token.equals("Q1") || token.equals("Q2")) && layer == 1)
-				|| ((!(token.equals("w0") || token.equals("W1") || token.equals("W2") || token.equals("f0")
+		if (((token.equals("w0") || token.equals("W1") || token.equals("F2") || token.equals("F1") || token.equals("f2")
+				|| token.equals("B0") || token.equals("Q1") || token.equals("Q2")) && layer == 1)
+				|| ((!(token.equals("w0") || token.equals("W1") || token.equals("W2") || token.equals("f2")
 						|| token.equals("F1") || token.equals("F2") || token.equals("B0") || token.equals("Q1")
 						|| token.equals("Q2"))) && layer == 3)) {
 			if (token.contains("w") || token.contains("W") || token.contains("B") || token.contains("Q")
