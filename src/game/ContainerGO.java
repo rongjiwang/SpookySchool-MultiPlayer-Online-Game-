5c6ce0d2456fbc67af148faff48819947267e22c
tezz99 <tezz995@gmail.com>
2016-10-07 23:10:49 +1300

    Started container game object implementation

---
+++ src/game/ContainerGO.java
@@ -0,0 +1,12 @@
+package game;
+
+public class ContainerGO extends InventoryGO {
+
+	public ContainerGO(String name, String id, String token, int size, String areaName, Position pos,
+			String description) {
+
+		super(name, id, token, size, areaName, pos, description);
+		// TODO Auto-generated constructor stub
+	}
+
+}
