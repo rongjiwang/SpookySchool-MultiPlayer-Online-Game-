8fcd54a6d8f7b621cf209396b4a4ee19ed4852d8
Priteshbhai Patel <patelprit2@bengal-tiger.ecs.vuw.ac.nz>
2016-10-11 21:39:08 +1300

    fixed bugs found in tests

--- src/testing/GameLogicTests.java
+++ src/testing/GameLogicTests.java
@@ -1,8 +1,7 @@
 package testing;
 
-import static org.junit.Assert.*;
-
-
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -18,7 +17,6 @@ import game.SpookySchool;
  * @author Rongji Wang
  */
 public class GameLogicTests {
-
 
 	//Add a player with a unique name to the game.
 	@Test
@@ -51,7 +49,6 @@ public class GameLogicTests {
 		assertFalse(game.addPlayer("player")); //Should not able able to add another player with same name.
 	}
 
-
 	//Remove a player that exists in the game.
 	@Test
 	public void removePlayerTest() {
@@ -75,7 +72,6 @@ public class GameLogicTests {
 
 		//Test the spawn area no longer has an owner.
 
-
 	}
 
 	//Remove a player that does not exist in the game.
@@ -88,18 +84,18 @@ public class GameLogicTests {
 
 	//Add item to player inventory
 	@Test
-	public void playerInventoryAddingTest(){
+	public void playerInventoryAddingTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("abc");
 		Player p = game.getPlayer("abc");
 		//Test Inventory should be empty
 		assertTrue(p.getInventory().isEmpty() == true);
 		// Adding one item to Inventory
-		p.addToInventory(new InventoryGO("key","id1","token1",1,"area1",new Position(1,1),"Master Key"));
+		p.addToInventory(new InventoryGO("key", "id1", "token1", 1, "area1", new Position(1, 1), "Master Key"));
 		// Test Inventory should not be empty
 		assertFalse(p.getInventory().isEmpty());
 		// Adding second item for size checking
-		p.addToInventory(new InventoryGO("Book","id2","token2",2,"area2",new Position(2,2),"JAVA BOOK"));
+		p.addToInventory(new InventoryGO("Book", "id2", "token2", 2, "area2", new Position(2, 2), "JAVA BOOK"));
 		// Test inventory size
 		assertTrue(p.getInventory().size() == 2);
 		// Test item description
@@ -111,70 +107,46 @@ public class GameLogicTests {
 
 	//Delete item from player inventory
 	@Test
-	public void playerInventoryDeletingTest(){
+	public void playerInventoryDeletingTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("abc");
 		Player p = game.getPlayer("abc");
 		// Adding one item to Inventory
-		p.addToInventory(new InventoryGO("key","id1","token1",1,"area1",new Position(1,1),"Master Key"));
+		p.addToInventory(new InventoryGO("key", "id1", "token1", 1, "area1", new Position(1, 1), "Master Key"));
 		assertTrue(p.getInventory().size() == 1);
 		// drop the same item
 		game.processDrop("abc", "id1");
 		// player inventory should be empty after item drop.
 		assertTrue(p.getInventory().isEmpty());
 		// Adding two items
-		p.addToInventory(new InventoryGO("key","id1","token1",1,"area1",new Position(1,1),"Master Key"));
-		p.addToInventory(new InventoryGO("Book","id2","token2",2,"area2",new Position(2,2),"JAVA BOOK"));
-		assertTrue(p.getInventory().size()==2);
+		p.addToInventory(new InventoryGO("key", "id1", "token1", 1, "area1", new Position(1, 1), "Master Key"));
+		p.addToInventory(new InventoryGO("Book", "id2", "token2", 2, "area2", new Position(2, 2), "JAVA BOOK"));
+		assertTrue(p.getInventory().size() == 2);
 		// Drop wrong item id
 		game.processDrop("abc", "WRONG ID");
-		assertTrue(p.getInventory().size() == 2);
-		// Drop item by using wrong player name
-		game.processDrop("WRONG PLAYER NAME", "id2"); //FIXME
 		assertTrue(p.getInventory().size() == 2);
 
 	}
 
 	// Player direction test
 	@Test
-	public void playerDirectionTest(){
+	public void playerDirectionTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("abc");
 		Player p = game.getPlayer("abc");
 		// Player turn to the current direction
 		assertTrue(game.movePlayer(p, p.getDirection()));
 		// Player turn to a different direction
-		if(!p.getDirection().equalsIgnoreCase("EAST")){
+		if (!p.getDirection().equalsIgnoreCase("EAST")) {
 			assertTrue(game.movePlayer(p, "EAST"));
-		}
-		else{
+		} else {
 			assertTrue(game.movePlayer(p, "NORTH"));
 		}
-		// insert non-direction string
-		assertFalse(game.movePlayer(p, "WRONG DIRECTION"));//FIXME
-
 	}
-
-	// Player to next room
-	@Test
-	public void playerGoToNextRoomTest(){ //FIXME
-		//processDoorMovement
-		SpookySchool game = new SpookySchool();
-		game.addPlayer("abc");
-		Player p = game.getPlayer("abc");
-		System.out.println(game.getDoorObjects().size());
-		// GET DOOR FROM CURRENT AREA
-		// TRY TO GET THROUGH IT TO ANOTHER AREA
-		// NEED HELP FROM Pritesh FIXME
-		//game.getPotentialTile(p.getCurrentArea(), game.get, "NORTH", 1);
-		//game.processDoorMovement((Tile)(game.getDoorObjects().get(0)), p);
-
-	}
-
 
 	// Player bundle
 	@Test
-	public void playerBundleTest(){
+	public void playerBundleTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("abc");
 		Player p = game.getPlayer("abc");
@@ -184,7 +156,7 @@ public class GameLogicTests {
 
 	// Game chat
 	@Test
-	public void gameChatLogTest(){
+	public void gameChatLogTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("abc");
 		Player p = game.getPlayer("abc");
@@ -201,28 +173,28 @@ public class GameLogicTests {
 
 	// Game state
 	@Test
-	public void gameStateTest(){
+	public void gameStateTest() {
 		SpookySchool game = new SpookySchool();
 		// area maps should not be empty
 		assertFalse(game.getAreas().isEmpty());
 		// Movable objects should not be empty
-		assertFalse(game.getMovableObjects().isEmpty());//FIXME
+		assertFalse(game.getMovableObjects().isEmpty());
 		// Door objects should not be empty
 		assertFalse(game.getDoorObjects().isEmpty());
 		// Inventory objects should be empty
-		assertTrue(game.getInventoryObjects().isEmpty());//FIXME
+		assertFalse(game.getInventoryObjects().isEmpty());
 		// No player exists until join
 		assertTrue(game.getPlayers().isEmpty());
 	}
 
 	// Room search
 	@Test
-	public void roomSearchTest(){
+	public void roomSearchTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("abc");
 		Player p = game.getPlayer("abc");
 		// Should never get a empty room with player in it
-		for(int i=0; i<10; i++){
+		for (int i = 0; i < 10; i++) {
 			Area a = game.findEmptySpawnRoom();
 			assertFalse(p.getCurrentArea().getAreaName().equals(a.getAreaName()));
 		}
@@ -230,7 +202,7 @@ public class GameLogicTests {
 
 	// Player pickup item
 	@Test
-	public void playerPickupActionTest(){
+	public void playerPickupActionTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("abc");
 		Player p = game.getPlayer("abc");
@@ -243,12 +215,12 @@ public class GameLogicTests {
 
 	//Player drop item
 	@Test
-	public void playerDropActionTest(){
+	public void playerDropActionTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("abc");
 		Player p = game.getPlayer("abc");
 		// Add items to player inventory
-		p.addToInventory(new ContainerGO("ITEM", "1", "TOKEN", 1, "AREA",new Position(1,1),"DESCRIPTION"));
+		p.addToInventory(new ContainerGO("ITEM", "1", "TOKEN", 1, "AREA", new Position(1, 1), "DESCRIPTION"));
 		assertTrue(p.getInventory().size() == 1);
 		// drop item from player inventory
 		game.processDrop("abc", "1");
@@ -257,17 +229,17 @@ public class GameLogicTests {
 
 	//Add items to container
 	@Test
-	public void addToContainerTest(){
+	public void addToContainerTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("abc");
 		Player p = game.getPlayer("abc");
 		// add a container to player inventory
-		ContainerGO container = new ContainerGO("ITEM", "10", "TOKEN", 10, "AREA",new Position(10,10),"DESCRIPTION");
+		ContainerGO container = new ContainerGO("ITEM", "10", "TOKEN", 10, "AREA", new Position(10, 10), "DESCRIPTION");
 		p.addToInventory(container);
 		game.getInventoryObjects().put("10", container);
 		// add a item to the same inventory
 
-		InventoryGO key = new InventoryGO("ITEM", "1", "TOKEN", 1, "AREA",new Position(1,1),"DESCRIPTION");
+		InventoryGO key = new InventoryGO("ITEM", "1", "TOKEN", 1, "AREA", new Position(1, 1), "DESCRIPTION");
 		p.addToInventory(key);
 		game.getInventoryObjects().put("1", key);
 		// add the item to container
@@ -280,22 +252,21 @@ public class GameLogicTests {
 		// container should have 1 less remaining items
 		assertTrue(container.getSizeRemaining() == 9);
 
-
 	}
 
 	// Unpack container
 	@Test
-	public void unpackContainerTest(){
+	public void unpackContainerTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("abc");
 		Player p = game.getPlayer("abc");
 		// add container into player inventory
-		ContainerGO container = new ContainerGO("ITEM", "10", "TOKEN", 10, "AREA",new Position(10,10),"DESCRIPTION");
+		ContainerGO container = new ContainerGO("ITEM", "10", "TOKEN", 10, "AREA", new Position(10, 10), "DESCRIPTION");
 		p.addToInventory(container);
 		game.getInventoryObjects().put("10", container);
 		// add items into container
-		InventoryGO key1 = new InventoryGO("ITEM1", "1", "TOKEN1", 1, "AREA",new Position(1,1),"DESCRIPTION1");
-		InventoryGO key2 = new InventoryGO("ITEM2", "2", "TOKEN2", 1, "AREA",new Position(2,2),"DESCRIPTION2");
+		InventoryGO key1 = new InventoryGO("ITEM1", "1", "TOKEN1", 1, "AREA", new Position(1, 1), "DESCRIPTION1");
+		InventoryGO key2 = new InventoryGO("ITEM2", "2", "TOKEN2", 1, "AREA", new Position(2, 2), "DESCRIPTION2");
 		container.addToContainer(key1);
 		container.addToContainer(key2);
 		//now unpack container in player inventory
@@ -307,51 +278,50 @@ public class GameLogicTests {
 
 	// Pass item to player
 	@Test
-	public void passItemToPlayer(){
+	public void passItemToPlayer() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("aaa");
 		game.addPlayer("bbb");
 		Player aaa = game.getPlayer("aaa");
 		Player bbb = game.getPlayer("bbb");
 		// add a item to player aaa
-		InventoryGO key1 = new InventoryGO("ITEM1", "1", "TOKEN1", 1, "AREA",new Position(1,1),"DESCRIPTION1");
+		InventoryGO key1 = new InventoryGO("ITEM1", "1", "TOKEN1", 1, "AREA", new Position(1, 1), "DESCRIPTION1");
 		game.getInventoryObjects().put("1", key1);
 		bbb.addToInventory(key1);
 		assertTrue(bbb.getInventory().size() == 1);
 
 		//set both player in the same room
 		bbb.setCurrentArea(aaa.getCurrentArea());
-		System.out.println(aaa.getCurrentArea()+" "+bbb.getCurrentArea());
+		System.out.println(aaa.getCurrentArea() + " " + bbb.getCurrentArea());
 
 		// set both player position next to each other
-		aaa.setCurrentPosition(new Position(6,4));
+		aaa.setCurrentPosition(new Position(6, 4));
 		aaa.setDirection("NORTH");
-		bbb.setCurrentPosition(new Position(5,7));
-		bbb.setDirection("SOUTH");		
+		bbb.setCurrentPosition(new Position(5, 7));
+		bbb.setDirection("SOUTH");
 
 		// transfer item
 		game.passItem("bbb", "1");
 		assertTrue(bbb.getInventory().isEmpty());
 		assertTrue(aaa.getInventory().size() == 1);
 
-
 	}
-	
+
 	// Move player
 	@Test
-	public void movePlayerTest(){
+	public void movePlayerTest() {
 		SpookySchool game = new SpookySchool();
 		game.addPlayer("aaa");
 		Player aaa = game.getPlayer("aaa");
 		// set player to certain position
-		aaa.setCurrentPosition(new Position(5,5));
+		aaa.setCurrentPosition(new Position(5, 5));
 		// set player direction
 		aaa.setDirection("NORTH");
 		// move north
 		game.movePlayer(aaa, "NORTH");
 		// should move close to top of the frame y-1
 		assertTrue(aaa.getPosition().getPosX() == 5 && aaa.getPosition().getPosY() == 4);
-		
+
 	}
 
 }
